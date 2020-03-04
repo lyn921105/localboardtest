1269e6db8f4c1c96b74ce3799e882f118cc5ad8c
seungwon lee <drugu66@gmail.com>
2020-03-04 09:30:13 +0900

    a

--- src/com/dao/BoardDAO.java
+++ src/com/dao/BoardDAO.java
@@ -13,15 +13,19 @@ import com.entity.BoardDTO;
 
 public class BoardDAO {
 	DataSource dataFactory;
+
 	public BoardDAO() { // 생성자
 		// DataSource 얻기, 커넥션 풀 사용
 		try {
 			Context ctx = new InitialContext();
-			dataFactory = (DataSource)ctx.lookup("java:comp/env/jdbc/Oracle11g");
-		} catch (Exception e) {e.printStackTrace();}
+			dataFactory = (DataSource) ctx.lookup("java:comp/env/jdbc/Oracle11g");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	} // end 생성자
-	// 목록보기
-	public ArrayList<BoardDTO> list(){
+		// 목록보기
+
+	public ArrayList<BoardDTO> list() {
 		ArrayList<BoardDTO> list = new ArrayList<>();
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -33,7 +37,7 @@ public class BoardDAO {
 					+ "order by repRoot desc, repStep asc";
 			pstmt = con.prepareStatement(query);
 			rs = pstmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				BoardDTO data = new BoardDTO();
 				data.setNum(rs.getInt("num"));
 				data.setAuthor(rs.getString("author"));
@@ -42,20 +46,56 @@ public class BoardDAO {
 				data.setReadcnt(rs.getInt("readcnt"));
 				data.setRepStep(rs.getInt("repStep"));
 				data.setRepIndent(rs.getInt("repIndent"));
-				
+
 				list.add(data);
-			}// end while
+			} // end while
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(rs!=null) rs.close();
-				if(pstmt!=null) pstmt.close();
-				if(con!=null) con.close();
-			} catch(SQLException e) {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return list;
 	} // end select
-} // end class
+		// end class
+
+	public void write(BoardDTO dto) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = dataFactory.getConnection();
+			StringBuffer query = new StringBuffer();
+			query.append("INSERT INTO board (num, title, author, content,");
+			query.append("repRoot, repStep, repIndent, passwd) values");
+			query.append("(board_seq.nextval,?,?,?,board_seq.currval,0,0,?)");
+			pstmt = con.prepareStatement(query.toString());
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getAuthor());
+			pstmt.setString(3, dto.getContent());
+			pstmt.setString(4, dto.getPasswd());
+			int n = pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
